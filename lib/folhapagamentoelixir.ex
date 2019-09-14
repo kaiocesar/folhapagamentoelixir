0fa7ca1ad5cc2046a916ef4af39924bc465edfe8
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-13 21:50:00 -0300

    Doctest and Tests

--- lib/folhapagamentoelixir.ex
+++ lib/folhapagamentoelixir.ex
@@ -4,15 +4,85 @@ defmodule Folhapagamentoelixir do
   """
 
   @doc """
-  Hello world.
+  Folha de pagamento com Elixir.
 
   ## Examples
 
-      iex> Folhapagamentoelixir.hello()
-      :world
+      iex> Folhapagamentoelixir.calcular_inss()
+      :inss
+
+      iex> Folhapagamentoelixir.calcular_fgts()
+      :fgts
+
+      iex> Folhapagamentoelixir.calcular_dependentes()
+      :dependentes
+
+      iex> Folhapagamentoelixir.calcular_irrf()
+      :irrf
+
+      iex> Folhapagamentoelixir.calcular_horas_extras()
+      :horas_extras
+
+      iex> Folhapagamentoelixir.calcular_dsr()
+      :dsr
+
+      iex> Folhapagamentoelixir.calcular_vale_transporte()
+      :vale_transporte
+
+      iex> Folhapagamentoelixir.calcular_vale_alimentacao()
+      :vale_alimentacao
+
+      iex> Folhapagamentoelixir.calcular_adicional_noturno()
+      :adicional_noturno
+
+      iex> Folhapagamentoelixir.calcular_adicional_insalubridade()
+      :adicional_insalubridade
+
+      iex> Folhapagamentoelixir.calcular_salario_liquido()
+      :salario_liquido
 
   """
-  def hello do
-    :world
+  def calcular_inss do
+    :inss
+  end
+
+  def calcular_fgts do
+    :fgts
+  end
+
+  def calcular_dependentes do
+    :dependentes
+  end
+
+  def calcular_irrf do
+    :irrf
+  end
+
+  def calcular_horas_extras do
+    :horas_extras
+  end
+
+  def calcular_dsr do
+    :dsr
+  end
+
+  def calcular_vale_transporte do
+    :vale_transporte
+  end
+
+  def calcular_vale_alimentacao do
+    :vale_alimentacao
+  end
+
+  def calcular_adicional_noturno do
+    :adicional_noturno
+  end
+
+  def calcular_adicional_insalubridade do
+    :adicional_insalubridade
+  end
+
+  def calcular_salario_liquido do
+    :salario_liquido
   end
 end
--- test/folhapagamentoelixir_test.exs
+++ test/folhapagamentoelixir_test.exs
@@ -2,7 +2,47 @@ defmodule FolhapagamentoelixirTest do
   use ExUnit.Case
   doctest Folhapagamentoelixir
 
-  test "greets the world" do
-    assert Folhapagamentoelixir.hello() == :world
+  test "Calcular INSS" do
+    assert Folhapagamentoelixir.calcular_inss() == :inss
+  end
+
+  test "Calcular FGTS" do
+    assert Folhapagamentoelixir.calcular_fgts() == :fgts
+  end
+
+  test "Calcular dependentes" do
+    assert Folhapagamentoelixir.calcular_dependentes() == :dependentes
+  end
+
+  test "Calcular IRRF" do
+    assert Folhapagamentoelixir.calcular_irrf() == :irrf
+  end
+
+  test "Calcular Horas Extras" do
+    assert Folhapagamentoelixir.calcular_horas_extras() == :horas_extras
+  end
+
+  test "Calcular DSR (Descanso Semanal Remunerado)" do
+    assert Folhapagamentoelixir.calcular_dsr() == :dsr
+  end
+
+  test "Calcular vale transporte" do
+    assert Folhapagamentoelixir.calcular_vale_transporte() == :vale_transporte
+  end
+
+  test "Calcular vale alimentação" do
+    assert Folhapagamentoelixir.calcular_vale_alimentacao() == :vale_alimentacao
+  end
+
+  test "Calcular adicional noturno" do
+    assert Folhapagamentoelixir.calcular_adicional_noturno() == :adicional_noturno
+  end
+
+  test "Calcular adicional insalubridade" do
+    assert Folhapagamentoelixir.calcular_adicional_insalubridade() == :adicional_insalubridade
+  end
+
+  test "Calcular Salario Liquido" do
+    assert Folhapagamentoelixir.calcular_salario_liquido() == :salario_liquido
   end
 end
