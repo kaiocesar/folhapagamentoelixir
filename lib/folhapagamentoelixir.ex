8135c58f7e696f1429370055a1e9bb2bbf04460d
Kaio Cesar <tecnico.kaio@gmail.com>
2019-09-13 21:34:02 -0300

    initial commit

---
+++ lib/folhapagamentoelixir.ex
@@ -0,0 +1,18 @@
+defmodule Folhapagamentoelixir do
+  @moduledoc """
+  Documentation for Folhapagamentoelixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Folhapagamentoelixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/folhapagamentoelixir_test.exs
@@ -0,0 +1,8 @@
+defmodule FolhapagamentoelixirTest do
+  use ExUnit.Case
+  doctest Folhapagamentoelixir
+
+  test "greets the world" do
+    assert Folhapagamentoelixir.hello() == :world
+  end
+end
